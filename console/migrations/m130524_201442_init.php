4851ec434a51b0657192fa9a968137c55083041e
xun4u <xunleee@qq.com>
2017-06-15 15:50:23 +0800

    migrate

--- console/migrations/m130524_201442_init.php
+++ console/migrations/m130524_201442_init.php
@@ -23,6 +23,8 @@ class m130524_201442_init extends Migration
             'status' => $this->smallInteger()->notNull()->defaultValue(10),
             'created_at' => $this->integer()->notNull(),
             'updated_at' => $this->integer()->notNull(),
+            'login_time'=>$this->integer(),
+            'login_ip'=>$this->integer(),
         ], $tableOptions);
     }
 
